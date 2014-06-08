a37532ecb756e22b4ed354a7d8f34f3d4ed708f3
Simon Horlick <simonhorlick@gmail.com>
2014-06-08 16:46:09 +0100

    Use config file for subscriptions

--- main.cc
+++ main.cc
@@ -23,6 +23,73 @@
 #include <condition_variable>
 #include <deque>
 
+fe_code_rate parse_fec(const std::string& str) {
+  if(str == "1/2") return FEC_1_2;
+  if(str == "2/3") return FEC_2_3;
+  if(str == "3/4") return FEC_3_4;
+  if(str == "4/5") return FEC_4_5;
+  if(str == "5/6") return FEC_5_6;
+  if(str == "6/7") return FEC_6_7;
+  if(str == "7/8") return FEC_7_8;
+  if(str == "8/9") return FEC_8_9;
+  if(str == "NONE") return FEC_NONE;
+  return FEC_AUTO;
+}
+
+fe_guard_interval parse_guard(const std::string& str) {
+  if(str == "1/16") return GUARD_INTERVAL_1_16;
+  if(str == "1/32") return GUARD_INTERVAL_1_32;
+  if(str == "1/4") return GUARD_INTERVAL_1_4;
+  if(str == "1/8") return GUARD_INTERVAL_1_8;
+  if(str == "1/128") return GUARD_INTERVAL_1_128;
+  return GUARD_INTERVAL_AUTO;
+}
+
+fe_modulation parse_constellation(const std::string& str) {
+  if(str == "QPSK") return QPSK;
+  if(str == "QAM128") return QAM_128;
+  if(str == "QAM16") return QAM_16;
+  if(str == "QAM256") return QAM_256;
+  if(str == "QAM32") return QAM_32;
+  if(str == "QAM64") return QAM_64;
+  return QAM_AUTO;
+}
+
+fe_transmit_mode parse_transmission(const std::string& str) {
+  if(str == "2k") return TRANSMISSION_MODE_2K;
+  if(str == "8k") return TRANSMISSION_MODE_8K;
+  if(str == "32k") return TRANSMISSION_MODE_32K;
+  return TRANSMISSION_MODE_AUTO;
+};
+
+bool parse_line(const std::string& line, long& frequency, fe_code_rate& fec, fe_modulation& constellation, fe_guard_interval& guard, fe_transmit_mode& transmission, std::string& identifier) {
+  // expecting something like this:
+  // T 546000000 8MHz  2/3 NONE   QAM256  32k 1/128 NONE # PSB3 BBCB
+
+  if(line.empty() || line[0] == '#') {
+    return false;
+  }
+
+  std::istringstream is(line);
+  std::string type, sfec, sconstellation, sguard, stransmission, _;
+  is >> _ >> frequency >> _ >> sfec >> _ >> sconstellation >> stransmission >> sguard >> _;
+
+  fec = parse_fec(sfec);
+  constellation = parse_constellation(sconstellation);
+  guard = parse_guard(sguard);
+  transmission = parse_transmission(stransmission);
+
+  // read end of line comment
+  is >> _;
+  if("#" == _) {
+    is >> _;
+    identifier = _;
+  }
+
+  return true;
+}
+
+
 struct buffer {
   uint8_t* buf;
   int len;
@@ -63,17 +130,18 @@ class blocking_queue {
 class file_writer {
   public:
 
-    file_writer(const char* file, int adapter, long frequency)
+    file_writer(const std::string& file, int adapter, long frequency, fe_code_rate fec, fe_modulation constellation, fe_guard_interval guard, fe_transmit_mode transmission)
       : stop_(false),
         done_(false),
         consumer_count_(0),
-        producer_count_(0) {
+        producer_count_(0),
+        file_(file) {
       consumer_thread_ = boost::thread(boost::bind(&file_writer::consume, this, file));
-      producer_thread_ = boost::thread(boost::bind(&file_writer::produce, this, adapter, frequency));
+      producer_thread_ = boost::thread(boost::bind(&file_writer::produce, this, adapter, frequency, fec, constellation, guard, transmission));
     }
 
-    int tune(LinuxDVBSource& source, int adapter, long frequency) {
-      if(!source.Tune(frequency, FEC_2_3, QAM_256, GUARD_INTERVAL_1_128, TRANSMISSION_MODE_32K)) {
+    int tune(LinuxDVBSource& source, long frequency, fe_code_rate fec, fe_modulation constellation, fe_guard_interval guard, fe_transmit_mode transmission) {
+      if(!source.Tune(frequency, fec, constellation, guard, transmission)) {
           std::cerr << "Failed to tune\n";
           return 1;
       }
@@ -85,10 +153,10 @@ class file_writer {
       return 0;
     }
 
-    void produce(int adapter, long frequency) {
+    void produce(int adapter, long frequency, fe_code_rate fec, fe_modulation constellation, fe_guard_interval guard, fe_transmit_mode transmission) {
       std::clog << "starting producer" << std::endl;
       LinuxDVBSource source(adapter,0);
-      if(tune(source, adapter, frequency)) {
+      if(tune(source, frequency, fec, constellation, guard, transmission)) {
         throw std::runtime_error("failed to tune");
       }
       while(!stop_) {
@@ -97,6 +165,7 @@ class file_writer {
         buf.len = source.Read(buf.buf, 188);
         if(buf.len) {
           // add to queue
+          if(stop_) done_ = true;
           queue_.push(buf);
           producer_count_ += buf.len;
         } else {
@@ -107,10 +176,10 @@ class file_writer {
       done_ = true;
     }
 
-    void consume(const char* file) {
+    void consume(const std::string& file) {
       std::clog << "starting consumer" << std::endl;
       // open file for writing
-      int output_fd = open(file, O_WRONLY|O_CREAT|O_TRUNC,
+      int output_fd = open(file.c_str(), O_WRONLY|O_CREAT|O_TRUNC,
           S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
       if(output_fd < 0) {
         throw std::runtime_error("error opening output file for writing");
@@ -120,6 +189,9 @@ class file_writer {
       while(!done_ || !queue_.empty()) {
         buffer buf = queue_.pop();
         write(output_fd, buf.buf, buf.len);
+
+        // TODO: Parse tables, print streams and other interesting information.
+
         delete buf.buf;
         consumer_count_ += buf.len;
       }
@@ -132,6 +204,10 @@ class file_writer {
     }
     long total_written() const {
       return consumer_count_;
+    }
+
+    const std::string& file() const {
+      return file_;
     }
 
     void join() {
@@ -154,6 +230,8 @@ class file_writer {
     boost::atomic<long> producer_count_;
 
     blocking_queue<buffer> queue_;
+
+    std::string file_;
 };
 
 sig_atomic_t signaled = 0;
@@ -162,15 +240,42 @@ void signal_handler(int sig) {
   signaled = 1;
 }
 
-void print_stats(const char* label, long read, long write, double s) {
+void print_stats(const std::string& label, long read, long write, double s) {
     double readmb = (read*8) / (1024*1024);
     double writemb = (write*8) / (1024*1024);
     std::cout
-      << label
+      << label << " "
       << "read " << readmb << " (" << (readmb/s) << "Mb/s) "
       << "written " << writemb << " (" << (writemb/s) << "Mb/s) "
       << "difference " << (read-write)/188 << " packets"
       << std::endl;
+}
+
+std::vector<file_writer*> read_config(const char* file) {
+  std::vector<file_writer*> writers;
+  int adapter = 0;
+
+  std::clog << "reading from " << file << std::endl;
+  std::ifstream conf(file);
+  if(!conf.is_open())
+    throw std::runtime_error("could not open file");
+
+  while(!conf.eof()) {
+    std::string line;
+    std::getline(conf,line);
+    long frequency;
+    fe_code_rate fec;
+    fe_modulation constellation;
+    fe_guard_interval guard;
+    fe_transmit_mode transmission;
+    std::string identifier;
+    if(parse_line(line, frequency, fec, constellation, guard, transmission, identifier)) {
+      std::clog << "subscribing to " << line << std::endl;
+      writers.push_back(new file_writer(identifier+".ts", adapter++, frequency, fec, constellation, guard, transmission));
+    }
+  }
+
+  return writers;
 }
 
 int main(int argc, char** argv) {
@@ -180,32 +285,31 @@ int main(int argc, char** argv) {
   // Handle ctrl-c
   signal(SIGINT, signal_handler);
 
-  // TODO: Parse channels.conf or initial tuning data
-  boost::posix_time::ptime start(boost::posix_time::microsec_clock::local_time());
-
   // TODO: Optimise read and write sizes
 
-  file_writer psb3("psb3.ts", 0, 545800000);
-  file_writer com7("com7.ts", 1, 570000000);
+  std::vector<file_writer*> writers = read_config("uk-CrystalPalace");
 
-  while(true) {
-    if(signaled) {
-      std::clog << "stopping threads" << std::endl;
-      psb3.stop();
-      com7.stop();
-      break;
-    }
+  boost::posix_time::ptime start(boost::posix_time::microsec_clock::local_time());
+  while(!signaled) {
     boost::posix_time::ptime now(boost::posix_time::microsec_clock::local_time());
     boost::posix_time::time_duration td(now-start);
     double s = td.total_microseconds() / 1e6;
-    print_stats("psb3", psb3.total_read(), psb3.total_written(), s);
-    print_stats("com7", com7.total_read(), com7.total_written(), s);
+    for(auto it = writers.begin(); it != writers.end(); ++it) {
+      print_stats((*it)->file(), (*it)->total_read(), (*it)->total_written(), s);
+    }
     sleep(1);
   }
 
+  std::clog << "stopping threads" << std::endl;
+  for(auto it = writers.begin(); it != writers.end(); ++it) {
+    (*it)->stop();
+  }
+
   std::clog << "joining threads" << std::endl;
-  psb3.join();
-  com7.join();
+  for(auto it = writers.begin(); it != writers.end(); ++it) {
+    (*it)->join();
+    delete *it;
+  }
 
   return 0;
 }
